4b7784386f175964d65ec29e80b50fd85e2d3020
fatmabenhlel1 <fatmaezzahra.benhelal@supcom.tn>
2024-12-02 10:54:25 +0100

    first commit

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 }
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({Key? key}) : super(key: key);
+  const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
